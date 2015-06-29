62809a2e4e69d4e640f61996479252af892f8432
DanielHeath1234 <minidan98@iinet.net.au>
2015-06-29 14:27:18 +1000

    Added Utils Folder
    Added:
    -Howler.js
    -Keyboard.js
    -Vector2.js
    
    Changed:
    -Player.js

--- Game/Scripts/Entity/Player.js
+++ Game/Scripts/Entity/Player.js
@@ -4,22 +4,26 @@ var context = canvas.getContext("2d");
 var thrust = document.createElement("img");
 thrust.src = 'Media/Art/thrust.png';
 
-var player = {
-	image : document.createElement("img"),
+var player = function(){
+	image = document.createElement("img"),
 	
-	width : 63,
-	height : 123,
+	width = 63,
+	height = 123,
 
-	x : canvas.width / 2,
-	y : canvas.height / 2,
+	x = canvas.width / 2,
+	y = canvas.height / 2,
 	
-	speed : 550,
-	angle : 0,
-	rotationSpeed : 2,
+	speed = 550,
+	angle = 0,
+	rotationSpeed = 2,
 	
-	directionX : 0,
-	directionY : 0,
-	angularVelocity : 0,
+	directionX = 0,
+	directionY = 0,
+	angularVelocity = 0,
 }
 
 player.image.src = 'Media/Art/ship.png';
+
+player.prototype.update = function(deltaTime){
+	
+}
