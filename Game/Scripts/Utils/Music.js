fffd52903e09a8771582288c25ebb14f8d1067b8
JeremyCraig <jemmies_knights@yahoo.com.au>
2015-06-30 16:39:14 +1000

    Fixed Music script
    now plays music (had to just simply rename a file)

--- Game/Scripts/Utils/Music.js
+++ Game/Scripts/Utils/Music.js
@@ -1,5 +1,5 @@
 var bgMusic = new Howl({
-	urls:["Media/Sounds/Hampton the Hampster - The Hampster Dance Song.ogg"],
+	urls:["Media/Sounds/ThemeMusic.ogg"],
 	loop:true,
 	buffer:true,
 	colume:0.05,
