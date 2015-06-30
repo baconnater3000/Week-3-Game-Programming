0c98deaa68524e3e36294d2c57b5f6a9071480fd
DanielHeath1234 <minidan98@iinet.net.au>
2015-06-30 10:14:51 +1000

    Changed Player a fair bit
    Keybinds in, but not working at this stage due to keybind file not
    working as needed.

---
+++ Game/Scripts/Entity/Player/PlayerKeys.js
@@ -0,0 +1,43 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+
+var keyboard = new Keyboard();
+
+var playerKeybinds = function(deltaTime){
+	if(keyboard.isKeyDown(keyboard.KEY_W) == true){
+		player.directionX = 0;
+		player.directionY = -1;
+		player.isMoving = true;
+	}else
+	
+	if(keyboard.isKeyDown(keyboard.KEY_A) == true){
+		player.angularVelocity = -player.rotationSpeed;
+	}else
+	
+	if(keyboard.isKeyDown(keyboard.KEY_S) == true){
+		player.directionX = 0;
+		player.directionY = 0.5;
+	}else
+	
+	if(keyboard.isKeyDown(keyboard.KEY_D) == true){
+		player.angularVelocity = player.rotationSpeed;
+	}
+	
+	
+	if(keyboard.onKeyUp(keyboard.KEY_W)){
+		player.directionY = 0;
+		player.isMoving = false;
+	}else
+	
+	if(keyboard.onKeyUp(keyboard.KEY_A)){
+		player.angularVelocity = 0;
+	}else
+	
+	if(keyboard.onKeyUp(keyboard.KEY_S)){
+		player.directionY = 0;
+	}else
+	
+	if(keyboard.onKeyUp(keyboard.KEY_D)){
+		player.angularVelocity = 0;
+	}
+}
