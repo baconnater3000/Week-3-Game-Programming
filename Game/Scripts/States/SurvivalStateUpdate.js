5e1f1403d3c548bfd671a828f5e06add3f7f9402
Ethan Holdsworth <ethanslh@gmail.com>
2015-07-02 09:22:26 +1000

    Added survival state

---
+++ Game/Scripts/States/SurvivalStateUpdate.js
@@ -0,0 +1,62 @@
+var survivalState = function()
+{
+	this.prototype = BaseState;
+}
+
+survivalState.prototype.load = function()
+{
+	
+}
+
+survivalState.prototype.unload = function()
+{
+	
+}
+
+survivalState.prototype.update = function(deltaTime)
+{
+	canvas.width = canvas.width;
+	context.drawImage(background, 0, 0);
+	
+	player.update(deltaTime);
+	player.draw();
+	
+	shop.update(deltaTime);
+	shop.draw();
+	
+	timer += deltaTime;
+
+	fpsTime += deltaTime;
+	fpsCount++;
+	if(fpsTime >= 1)
+	{
+		fpsTime -= 1;
+		fps = fpsCount;
+		fpsCount = 0;
+	}		
+	
+	enemyManager.update(deltaTime);
+	enemyManager.draw();
+	enemyManager.enemiesOnScreen();
+	
+	if(player.isDead == true)
+	{
+		stateManager.switchState(new LoseState());
+	}
+	
+	if(player.isDead == false && player.score == 50000)
+	{
+		player.score += 1000;
+		stateManager.switchState(new WinState());
+	}
+	
+	if(keyboard.isKeyDown(keyboard.KEY_ESCAPE) == true)
+	{
+		stateManager.switchState(new PauseState());
+	}
+}
+
+survivalState.prototype.draw = function()
+{
+	
+}
