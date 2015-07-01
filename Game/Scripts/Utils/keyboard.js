e31a76caac4101b3ef6177b1cb6cad025cae839c
baconnater3000 <bgame58@hotmail.com>
2015-07-01 16:27:44 +1000

    Fixed Keyboard

--- Game/Scripts/Utils/keyboard.js
+++ Game/Scripts/Utils/keyboard.js
@@ -22,12 +22,11 @@ var Keyboard = function() {
 	this.KEY_W = 87;
 	this.KEY_SHIFT = 16;
 	
-	this.KEY_I = 73;
-	this.KEY_R = 82;
-	this.KEY_Q = 81;
 	this.KEY_ESCAPE = 27;
 	this.KEY_BACKSPACE = 8;
-	this.KEY_ENTER = 13;
+	this.KEY_I = 73;
+	this.KEY_R = 81;
+	this.KEY_Q = 82;
 };
 
 Keyboard.prototype.onKeyDown = function(evt){
