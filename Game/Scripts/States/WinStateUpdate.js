899d54d8ca13b73f308ba1ca6ba02602e027defb
baconnater3000 <bgame58@hotmail.com>
2015-07-02 13:38:37 +1000

    Finished Updating UI

--- Game/Scripts/States/WinStateUpdate.js
+++ Game/Scripts/States/WinStateUpdate.js
@@ -15,6 +15,25 @@ WinState.prototype.unload = function()
 
 WinState.prototype.update = function(deltaTime)
 {
+	if(player.mouseClicked)
+	{
+		if(player.mousePos.x >= canvas.width / 2 - 50 && player.mousePos.x <= canvas.width / 2 + 50)
+		{
+			//Resume Button
+			if(player.mousePos.y >= canvas.height / 2 + 60 && player.mousePos.y <= canvas.height / 2 + 60 + 25)
+			{
+				stateManager.switchState(new GameState());
+			}
+			
+			//Quit Button
+			if(player.mousePos.y >= canvas.height / 2 + 90 && player.mousePos.y <= canvas.height / 2 + 90 + 25)
+			{
+				location.reload();
+			}
+		}
+		player.mouseClicked = false;
+	}
+	
 	if(keyboard.isKeyDown(keyboard.KEY_SPACE) == true)
 	{
 		stateManager.switchState(new GameState());
@@ -34,6 +53,12 @@ WinState.prototype.draw = function()
 	canvas.width = canvas.width;
 	context.drawImage(background, 0, 0);
 	
+	/*
+	context.strokeStyle = "White";
+	context.strokeRect(canvas.width / 2 - 50, canvas.height / 2 + 60, 100, 25);
+	context.strokeRect(canvas.width / 2 - 50, canvas.height / 2 + 90, 100, 25);
+	*/
+	
 	var Title = document.createElement("img");
 	Title.src = "Media/Art/WinText.png";
 	var center = context.measureText(Title);
@@ -45,11 +70,11 @@ WinState.prototype.draw = function()
 	center = context.measureText(contText);
 	context.fillText(contText, canvas.width / 2 - center.width / 2, canvas.height / 2 + 50);
 
-	contText = "Press 'Space' To Continue Playing";
+	contText = "Resume";
 	center = context.measureText(contText);
 	context.fillText(contText, canvas.width / 2 - center.width / 2, canvas.height / 2 + 80);
 	
-	contText = "Press 'Esc' To Quit The Game";
+	contText = "Quit";
 	center = context.measureText(contText);
 	context.fillText(contText, canvas.width / 2 - center.width / 2, canvas.height / 2 + 110);
 
