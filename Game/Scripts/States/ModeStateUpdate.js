681d7c9e1088e947f65454a3a56ab090f11850c8
baconnater3000 <bgame58@hotmail.com>
2015-07-02 11:50:44 +1000

    Updated UI

--- Game/Scripts/States/ModeStateUpdate.js
+++ Game/Scripts/States/ModeStateUpdate.js
@@ -1,21 +1,21 @@
-var DiffState = function()
+var ModeState = function()
 {
 	canvas.addEventListener("mousedown", this.getPosition, false);
 
 	this.prototype = BaseState;
 }
 
-DiffState.prototype.load = function() 
+ModeState.prototype.load = function() 
 {
 	
 }
 
-DiffState.prototype.unload = function()
+ModeState.prototype.unload = function()
 {
 	
 }
 
-DiffState.prototype.getPosition = function(evt){
+ModeState.prototype.getPosition = function(evt){
 	var x = evt.x;
 	var y = evt.y;
 	
@@ -29,45 +29,35 @@ DiffState.prototype.getPosition = function(evt){
 	player.mouseClicked = true;
 }
 
-DiffState.prototype.update = function(deltaTime)
+ModeState.prototype.update = function(deltaTime)
 {
-	if(player.mouseClicked){
-		if(player.mousePos.x >= canvas.width / 2 - 200 && player.mousePos.x <= canvas.width / 2 + 200){
+	if(player.mouseClicked)
+	{
+		if(player.mousePos.x >= canvas.width / 2 - 100 && player.mousePos.x <= canvas.width / 2 + 100)
+		{
 			//GM1 BUTTON
-			if(player.mousePos.y >= 180 && player.mousePos.y <= 180 + 25){
+			if(player.mousePos.y >= 180 && player.mousePos.y <= 180 + 25)
+			{
 				stateManager.switchState(new GameState());
 			}
 			
 			//GM2 BUTTON
-			if(player.mousePos.y >= 220 && player.mousePos.y <= 220 + 25){
-				stateManager.switchState(new GameState());
+			if(player.mousePos.y >= 220 && player.mousePos.y <= 220 + 25)
+			{
+				stateManager.switchState(new SurvivalState());
 			}
 			
 			//BACK BUTTON
-			if(player.mousePos.y >= 260 && player.mousePos.y <= 260 + 25){
+			if(player.mousePos.y >= 260 && player.mousePos.y <= 260 + 25)
+			{
 				stateManager.switchState(new SplashState());
 			}
 		}
 		player.mouseClicked = false;
 	}
-
-	if(keyboard.isKeyDown(keyboard.KEY_1) == true)
-	{
-		stateManager.switchState(new GameState());
-	}
-	
-	if(keyboard.isKeyDown(keyboard.KEY_2) == true)
-	{
-		stateManager.switchState(new GameState());
-	}
-	
-	if(keyboard.isKeyDown(keyboard.KEY_BACKSPACE) == true)
-	{
-		stateManager.switchState(new SplashState());
-	}
 }
 
-DiffState.prototype.draw = function()
+ModeState.prototype.draw = function()
 {
 	context.canvas.width = window.innerWidth - 20;
 	context.canvas.height = window.innerHeight - 20;
@@ -75,11 +65,11 @@ DiffState.prototype.draw = function()
 	canvas.width = canvas.width;
 	context.drawImage(background, 0, 0);
 	
-	/*
+	/*n 
 	context.strokeStyle = "White";
-	context.strokeRect(canvas.width / 2 - 200, 180, 400, 25);
-	context.strokeRect(canvas.width / 2 - 200, 220, 400, 25);
-	context.strokeRect(canvas.width / 2 - 200, 260, 400, 25);
+	context.strokeRect(canvas.width / 2 - 100, 180, 200, 25);
+	context.strokeRect(canvas.width / 2 - 100, 220, 200, 25);
+	context.strokeRect(canvas.width / 2 - 100, 260, 200, 25);
 	*/
 	
 	var Title = document.createElement("img");
@@ -89,15 +79,15 @@ DiffState.prototype.draw = function()
 	
 	context.fillStyle = "white";
 	context.font = "25px Cooper Black";
-	var contText = "Press '1' To Play On Wave Mode";
+	var contText = "Wave Mode";
 	center = context.measureText(contText);
 	context.fillText(contText, canvas.width / 2 - center.width / 2, 200);
 
-	contText = "Press '2' To Play On Survival Mode";
+	contText = "Survival Mode";
 	center = context.measureText(contText);
 	context.fillText(contText, canvas.width / 2 - center.width / 2, 240);
 	
-	contText = "Press 'Backspace' To Go Back";
+	contText = "Go Back";
 	center = context.measureText(contText);
 	context.fillText(contText, canvas.width / 2 - center.width / 2, 280);
 }
